63d6296f3deab61d030d36aac86312d9b895f528
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-19 19:43:46 -0400

    camera: Add alternate unix socket path

--- camera/QCamera2/stack/mm-camera-interface/src/mm_camera_sock.c
+++ camera/QCamera2/stack/mm-camera-interface/src/mm_camera_sock.c
@@ -76,9 +76,19 @@ int mm_camera_socket_create(int cam_id, mm_camera_sock_type_t sock_type)
         cam_id);
     rc = connect(socket_fd, &sock_addr.addr, sizeof(sock_addr.addr_un));
     if (0 != rc) {
-      close(socket_fd);
-      socket_fd = -1;
-      CDBG_ERROR("%s: socket_fd=%d %s ", __func__, socket_fd, strerror(errno));
+        CDBG_ERROR("%s: Connecting to %s failed: %s ", __func__, sock_addr.addr_un.sun_path, strerror(errno));
+        CDBG_ERROR("%s: socket_fd=%d %s ", __func__, socket_fd, strerror(errno));
+        memset(&sock_addr, 0, sizeof(sock_addr));
+        sock_addr.addr_un.sun_family = AF_UNIX;
+        snprintf(sock_addr.addr_un.sun_path, UNIX_PATH_MAX, "/data/cam_socket%d",
+          cam_id);
+        rc = connect(socket_fd, &sock_addr.addr, sizeof(sock_addr.addr_un));
+
+        if (0 != rc) {
+          close(socket_fd);
+          socket_fd = -1;
+          CDBG_ERROR("%s: socket_fd=%d %s ", __func__, socket_fd, strerror(errno));
+        }
     }
 
     CDBG("%s: socket_fd=%d %s", __func__, socket_fd,
