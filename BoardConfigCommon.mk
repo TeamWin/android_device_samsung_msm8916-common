202021c36e6c26b39a32e8f62ecc0dddb8776e0e
palmbeach05 <davmcleod87@gmail.com>
2020-02-03 22:18:10 -0500

    Update to add internal storage backup/restore, flashable system,recovery, and boot img options. Includes all changes previously made at https://gerrit.twrp.me/c/android_device_samsung_msm8916-common/+/1893
    Change-Id: I555ac6037be8a0d4dc0ba65be8efa3a8ca342993

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -168,10 +168,10 @@ endif
 #ifneq ($(wildcard bootable/recovery-twrp),)
 #	RECOVERY_VARIANT := twrp
 #endif
-
+ 
 # SELinux
 # include device/qcom/sepolicy/sepolicy.mk
 # include device/qcom/sepolicy/legacy-sepolicy.mk
-
+ 
 #BOARD_SEPOLICY_DIRS += \
 #    $(LOCAL_PATH)/sepolicy
