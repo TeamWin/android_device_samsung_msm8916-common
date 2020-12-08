dc6a17d736e67d25fae6282a54198d2b80396707
palmbeach05 <davmcleod87@gmail.com>
2020-12-07 23:41:00 -0500

    msm8916-common: Switch to cryptfs_hw hidl service

--- msm8916.mk
+++ msm8916.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+    
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
