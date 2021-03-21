82964cbf2b270b682b7ef64d222466fbe1adf071
palmbeach05 <davmcleod87@gmail.com>
2021-03-21 15:46:22 -0400

    msm8916-common: Switch to keymaster 4.0

--- msm8916.mk
+++ msm8916.mk
@@ -248,6 +248,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-impl \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
     android.hardware.authsecret@1.0-service \
